40dfb95962f0bd9b1c2cd8547aafe2448ff59cfa
Eric Chu <chufucious@gmail.com>
2015-09-29 15:16:55 -0400

    this dont work

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,20 @@ var gulp = require('gulp');
 var imagemin = require('gulp-imagemin');
 var pngquant = require('imagemin-pngquant');
 
-var inputPath = '';
-var outputPath = '';
+// gulp.task('copy', ['clean'], function () {
+//       return gulp.src(['src/ios/**/*', 'src/android/**/*'], {
+//         base: 'src'
 
-gulp.task('default', function () {
-    return gulp.src('src/images/*')
-        .pipe(imagemin({
-            progressive: true,
-            svgoPlugins: [{removeViewBox: false}],
-            use: [pngquant()]
-        }))
-        .pipe(gulp.dest('dist/images'));
-});
+//       // .pipe(imagemin({
+//       //     progressive: true,
+//       //     svgoPlugins: [{removeViewBox: false}],
+//       //     use: [pngquant()]
+//       // }))
+//       .pipe(gulp.dest('build'));
+// });
+
+          gulp.task('copy', ['clean'], function () {
+        return gulp.src(['src/public/**/*', 'src/vendor/**/*'], {
+            base: 'src'
+        }).pipe(gulp.dest('build'));
+    });
