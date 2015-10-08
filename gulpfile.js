c3db1de93cb30034d264f245850dd7f62b484b5d
Eric Chu <chufucious@gmail.com>
2015-10-08 11:11:49 -0400

    optimize works

--- gulpfile.js
+++ gulpfile.js
@@ -2,20 +2,28 @@ var gulp = require('gulp');
 var imagemin = require('gulp-imagemin');
 var pngquant = require('imagemin-pngquant');
 
-// gulp.task('copy', ['clean'], function () {
-//       return gulp.src(['src/ios/**/*', 'src/android/**/*'], {
-//         base: 'src'
+gulp.task('optimize', function () {
+    return gulp.src('src/**')
+        .pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()]
+        }))
+        .pipe(gulp.dest('build'));
+});
 
-//       // .pipe(imagemin({
-//       //     progressive: true,
-//       //     svgoPlugins: [{removeViewBox: false}],
-//       //     use: [pngquant()]
-//       // }))
-//       .pipe(gulp.dest('build'));
-// });
+gulp.task('move-android', ['optimize'], function() {
+  return gulp.src('build/android/**')
+    .pipe(gulp.dest('/Users/eric/projects/current-android/current/src/main/res'));
+});
 
-          gulp.task('copy', ['clean'], function () {
-        return gulp.src(['src/public/**/*', 'src/vendor/**/*'], {
-            base: 'src'
-        }).pipe(gulp.dest('build'));
-    });
+gulp.task('move-ios', ['optimize'], function() {
+  return gulp.src('build/ios/**')
+    .pipe(gulp.dest('/Users/eric/projects/capturelive-ios-assets/assets'));
+});
+
+// /Users/eric/projects/current-android/current/src/main/res
+// /Users/eric/projects/capturelive-ios-assets/assets
+
+gulp.task('default', ['optimize', 'move-android', 'move-ios']);
+// gulp.task('default', ['one', 'two']);
