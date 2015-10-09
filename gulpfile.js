1bf4ded4d0494c4ab36dc107edc25d0354ad9364
Eric Chu <chufucious@gmail.com>
2015-10-09 15:53:19 -0400

    changed var names to path

--- gulpfile.js
+++ gulpfile.js
@@ -5,9 +5,9 @@ var pngquant = require('imagemin-pngquant');
 // todo: android directories: add drawable to gulp.dest
 // todo: http://stackoverflow.com/questions/26784094/can-i-use-a-gulp-task-with-multiple-sources-and-multiple-destinations
 
-var sourceDirectory = '/Users/eric/Dropbox/Capture/design/mobile/_output/_sliced/current/src';
-var iosOutputDirectory = '/Users/eric/projects/capturelive-ios-assets/assets';
-var androidOutputDirectory = '/Users/eric/projects/current-android/current/src/main/res';
+var sourcePath = '/Users/eric/Dropbox/Capture/design/mobile/_output/_sliced/current/src';
+var iosOutputPath = '/Users/eric/projects/capturelive-ios-assets/assets';
+var androidOutputPath = '/Users/eric/projects/current-android/current/src/main/res';
 
 // drawable-hdpi
 // drawable-mdpi
@@ -16,7 +16,7 @@ var androidOutputDirectory = '/Users/eric/projects/current-android/current/src/m
 // drawable-xxxhdpi
 
 gulp.task('optimize', function () {
-  return gulp.src(sourceDirectory+'/**')
+  return gulp.src(sourcePath+'/**')
     .pipe(imagemin({
         progressive: true,
         svgoPlugins: [{removeViewBox: false}],
@@ -27,12 +27,12 @@ gulp.task('optimize', function () {
 
 gulp.task('move-android', ['optimize'], function() {
   return gulp.src('build/android/**')
-    .pipe(gulp.dest(androidOutputDirectory));
+    .pipe(gulp.dest(androidOutputPath));
 });
 
 gulp.task('move-ios', ['optimize'], function() {
   return gulp.src('build/ios/**')
-    .pipe(gulp.dest(iosOutputDirectory));
+    .pipe(gulp.dest(iosOutputPath));
 });
 
 gulp.task('default', ['optimize', 'move-android', 'move-ios']);
