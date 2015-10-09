9485a81939c7349b855b5d0e0514d429877601e7
Eric Chu <chufucious@gmail.com>
2015-10-09 16:02:54 -0400

    now moves android assets to drawable output dirs

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var imagemin = require('gulp-imagemin');
 var pngquant = require('imagemin-pngquant');
+var merge = require('merge-stream');
 
 // todo: android directories: add drawable to gulp.dest
 // todo: http://stackoverflow.com/questions/26784094/can-i-use-a-gulp-task-with-multiple-sources-and-multiple-destinations
@@ -26,8 +27,23 @@ gulp.task('optimize', function () {
 });
 
 gulp.task('move-android', ['optimize'], function() {
-  return gulp.src('build/android/**')
-    .pipe(gulp.dest(androidOutputPath));
+
+  var hdpi = gulp.src('build/android/hdpi/**')
+    .pipe(gulp.dest(androidOutputPath+'/drawable-hdpi'));
+
+  var mdpi = gulp.src('build/android/mdpi/**')
+    .pipe(gulp.dest(androidOutputPath+'/drawable-mdpi'));
+
+  var xhdpi = gulp.src('build/android/xhdpi/**')
+    .pipe(gulp.dest(androidOutputPath+'/drawable-xhdpi'));
+
+  var xxhdpi = gulp.src('build/android/xxhdpi/**')
+    .pipe(gulp.dest(androidOutputPath+'/drawable-xxhdpi'));
+
+  var xxxhdpi = gulp.src('build/android/xxxhdpi/**')
+    .pipe(gulp.dest(androidOutputPath+'/drawable-xxxhdpi'));
+
+  return merge(hdpi, mdpi, xhdpi, xxhdpi, xxxhdpi);
 });
 
 gulp.task('move-ios', ['optimize'], function() {
