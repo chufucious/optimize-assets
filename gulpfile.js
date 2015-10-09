9ff5431b3f26a30685d7ca478fa7af7324a09bde
Eric Chu <chufucious@gmail.com>
2015-10-09 14:50:22 -0400

    updated some code

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,12 @@ var gulp = require('gulp');
 var imagemin = require('gulp-imagemin');
 var pngquant = require('imagemin-pngquant');
 
-
-// todo: change file path for src directory (hard code it)
-// todo: add variable names for dirs
 // todo: android directories: add drawable to gulp.dest
 // todo: http://stackoverflow.com/questions/26784094/can-i-use-a-gulp-task-with-multiple-sources-and-multiple-destinations
 
 var sourceDirectory = '/Users/eric/Dropbox/Capture/design/mobile/_output/_sliced/current/src';
-var iosAssetDirectory = '';
-var androidAssetDirectory = '';
+var iosOutputDirectory = '/Users/eric/projects/capturelive-ios-assets/assets';
+var androidOutputDirectory = '/Users/eric/projects/current-android/current/src/main/res';
 
 // drawable-hdpi
 // drawable-mdpi
@@ -30,16 +27,12 @@ gulp.task('optimize', function () {
 
 gulp.task('move-android', ['optimize'], function() {
   return gulp.src('build/android/**')
-    .pipe(gulp.dest('/Users/eric/projects/current-android/current/src/main/res'));
+    .pipe(gulp.dest(androidOutputDirectory));
 });
 
 gulp.task('move-ios', ['optimize'], function() {
   return gulp.src('build/ios/**')
-    .pipe(gulp.dest('/Users/eric/projects/capturelive-ios-assets/assets'));
+    .pipe(gulp.dest(iosOutputDirectory));
 });
 
-// /Users/eric/projects/current-android/current/src/main/res
-// /Users/eric/projects/capturelive-ios-assets/assets
-
 gulp.task('default', ['optimize', 'move-android', 'move-ios']);
-// gulp.task('default', ['one', 'two']);
