585a3c451314f28d08dde335c28d4ba692e5a77f
Eric Chu <chufucious@gmail.com>
2015-10-09 16:03:36 -0400

    removed todos

--- gulpfile.js
+++ gulpfile.js
@@ -3,18 +3,9 @@ var imagemin = require('gulp-imagemin');
 var pngquant = require('imagemin-pngquant');
 var merge = require('merge-stream');
 
-// todo: android directories: add drawable to gulp.dest
-// todo: http://stackoverflow.com/questions/26784094/can-i-use-a-gulp-task-with-multiple-sources-and-multiple-destinations
-
 var sourcePath = '/Users/eric/Dropbox/Capture/design/mobile/_output/_sliced/current/src';
 var iosOutputPath = '/Users/eric/projects/capturelive-ios-assets/assets';
 var androidOutputPath = '/Users/eric/projects/current-android/current/src/main/res';
-
-// drawable-hdpi
-// drawable-mdpi
-// drawable-xhdpi
-// drawable-xxhdpi
-// drawable-xxxhdpi
 
 gulp.task('optimize', function () {
   return gulp.src(sourcePath+'/**')
