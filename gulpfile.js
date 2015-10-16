4241457b7edbaf853e1264443f538663c69d704a
Eric Chu <chufucious@gmail.com>
2015-10-16 14:19:27 -0400

    added config file

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,10 @@ var gulp = require('gulp');
 var imagemin = require('gulp-imagemin');
 var pngquant = require('imagemin-pngquant');
 var outputType = ['hdpi', 'mdpi', 'xhdpi', 'xxhdpi', 'xxxhdpi'];
-var config = "./config.json";
+var config = require("./config.json");
 
 gulp.task('optimize', function () {
-  return gulp.src(sourcePath+'/**')
+  return gulp.src(config.source+'/**')
     .pipe(imagemin({
         progressive: true,
         svgoPlugins: [{removeViewBox: false}],
@@ -14,20 +14,21 @@ gulp.task('optimize', function () {
     .pipe(gulp.dest('build'));
 });
 
-var moveAsset = function(type){
+var moveAsset = function (type){
   return gulp.src('build/android/'+type+'/**')
-    .pipe(gulp.dest(androidOutputPath+'/drawable-'+type));
+    .pipe(gulp.dest(config.android+'/drawable-'+type));
 };
 
-gulp.task('move-android', ['optimize'], function() {
-  return outputType.forEach(function(type){
+gulp.task('move-android', ['optimize'], function () {
+  return outputType.forEach(function (type){
       return moveAsset(type);
     });
 });
 
-gulp.task('move-ios', ['optimize'], function() {
+gulp.task('move-ios', ['optimize'], function () {
+  console.log("hi tyler:",config.ios);
   return gulp.src('build/ios/**')
-    .pipe(gulp.dest(iosOutputPath));
+    .pipe(gulp.dest(config.ios));
 });
 
 gulp.task('default', ['optimize', 'move-android', 'move-ios']);
